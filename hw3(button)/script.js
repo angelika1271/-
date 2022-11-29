63d558e2b4a0bd191e526d3a15c42d4e8d89a79a
angelika127 <bai.21@uni-dubna.ru>
2022-11-29 16:06:53 +0300

    11

--- hw3(button)/script.js
+++ hw3(button)/script.js
@@ -1,18 +1,18 @@
-const random = (min, max) => {
-    const rand = min + Math.random() * (max - min + 1);
-    return Math.floor(rand);
+const random = (min, max) => { // задаём константу с рандомным значением между min и max
+    const rand = min + Math.random() * (max - min + 1); //к min добавляем случайное значение между min и max
+    return Math.floor(rand);  //возвращаем целое значение rand
 }
 
 
-const btn = document.querySelector('#btn');
+const btn = document.querySelector('#btn'); //возвращает элемент документа, который соответствует селектору #btn
 
-btn.addEventListener('mouseenter', () => {
+btn.addEventListener('mouseenter', () => {//при наведении мыши изменяем её положение на рандомные значения в % относительно экрана
     btn.style.left = `${random(0, 90)}%`;
     btn.style.top = `${random(0, 90)}%`;
 });
 
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', () => {// при нажатии на кнопку выводим текст
     alert('Congrats! You hit the button! What are you doing with your life? :D');
 });
 
